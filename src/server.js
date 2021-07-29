4e36efdf27f6b25612101def22168fa9874580a2
kleber castro <kleber.cristian@hotmail.com>
2021-07-29 00:47:50 -0300

    Deploy

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-const express = riquire('express')
-
-const server = express()
-
-server.listen(5000, () => console.log('Server started'))
-=======
 const express = require('express');
 const route = require('./route');
 const path = require('path');
 
-const server = express();
+const server = express()
 
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, 'views'));
@@ -17,5 +10,4 @@ server.set('views', path.join(__dirname, 'views'));
 server.use(express.static('public'));
 server.use(route);
 
-server.listen(8080, ()=> console.log('Server started'));
->>>>>>> e475daeb3e0160366ebd9169143ba23f6de9a9c1
+server.listen(8080, () => console.log('Server started'));
