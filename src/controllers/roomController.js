02bac48b6d61a59b0160bc7c39e502b12ed0ee5b
kleber Castro <kleber.cristian@hotmail.com>
2021-10-05 21:16:06 -0300

    Update

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,6 +1,36 @@
+const Database = require("../database/config");
+
 module.exports = {
-    create(req, res) {
-        let roomId = 132455
-        res.redirect(`/room/${roomId}`);        
+    async create(req, res) {
+        const db = await Database();
+        const pass = req.body.password;
+        let roomId = [];
+        let isRoom = true;
+
+        while(isRoom) {
+
+            // generate room number
+            for (var index = 0; index < 6; index++) {
+            roomId += Math.floor(Math.random() * 10).toString();
+            }
+
+            // Check room number
+            const roomExistIds =  await db.all(`SELEC * FROM rooms `);
+            isRoom = roomExistIds.some(roomExistId => roomExistId === roomId); 
+            if(!isRoom) {
+                // Insert room in the dabase
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`);
+
+                await db.close();
+
+                res.redirect(`/room/${roomId}`);    
+            }    
+        }
     }
 }
